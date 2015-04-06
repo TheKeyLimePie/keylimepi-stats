44c1c87eedea3b282cac91b2b1b63592aa0c33af
KeyLimePie <v.asakura@ymail.com>
2015-04-06 16:45:23 +0200

    improved sync settings

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -3,6 +3,7 @@ amountBig = 50;
 amountDSL = 96;	//speedtest recorded every 15 min -> 4*24 = 96
 interval = 0;
 intervalDSL = 0;
+sync = true;
 
 function init()
 {
@@ -193,127 +194,138 @@ function pad(number, digits)
 }
 function update()
 {
-	$.get("stats.php", function(val)
+	if(sync)
 	{
-		val = $.parseJSON(val);
-		var d = new Date();
-		//temperature
-		var date = pad(d.getHours(), 2).concat(":", pad(d.getMinutes(), 2), ":", pad(d.getSeconds(), 2));
-		var temp = [(parseInt(val.temp / 10)) / 100];
-		console.log("Temperature: " + temp + " deg C");
-		chartTemp.addData(temp, date);
-		if(chartTemp.datasets[0].points.length > amountBig)
-			chartTemp.removeData();
-		//CPU
-		var cpu = val.cpu[0];
-		var cpu0 = val.cpu[1];
-		var cpu1 = val.cpu[2];
-		var cpu2 = val.cpu[3];
-		var cpu3 = val.cpu[4];
-		
-		var cpu_busy = (parseInt(cpu[1]) + parseInt(cpu[2]) + parseInt(cpu[3]));
-		var cpu_idle = parseInt(cpu[4]);
-		var cpu_busy_perc = parseInt(cpu_busy / (cpu_idle + cpu_busy) * 100);
-		var cpu0_busy = (parseInt(cpu0[1]) + parseInt(cpu0[2]) + parseInt(cpu0[3]));
-		var cpu0_idle = parseInt(cpu0[4]);
-		var cpu0_busy_perc = parseInt(cpu0_busy / (cpu0_idle + cpu0_busy) * 100);
-		var cpu1_busy = parseInt(cpu1[1]) + parseInt(cpu1[2]) + parseInt(cpu1[3]);
-		var cpu1_idle = parseInt(cpu1[4]);
-		var cpu1_busy_perc = parseInt(cpu1_busy / (cpu1_idle + cpu1_busy) * 100);
-		var cpu2_busy = parseInt(cpu2[1]) + parseInt(cpu2[2]) + parseInt(cpu2[3]);
-		var cpu2_idle = parseInt(cpu2[4]);
-		var cpu2_busy_perc = parseInt(cpu2_busy / (cpu2_idle + cpu2_busy) * 100);
-		var cpu3_busy = parseInt(cpu3[1]) + parseInt(cpu3[2]) + parseInt(cpu3[3]);
-		var cpu3_idle = parseInt(cpu3[4]);
-		var cpu3_busy_perc = parseInt(cpu3_busy / (cpu3_idle + cpu3_busy) * 100);
-		
-		console.log("cpu: " + cpu_busy_perc + "% | " + "cpu0: " + cpu0_busy_perc + "% | " + "cpu1: " + cpu1_busy_perc + "% | " + "cpu2: " + cpu2_busy_perc + "% | " + "cpu3: " + cpu3_busy_perc + "%");
-		//idle
-		chartCPUCores.datasets[0].bars[0].value = 100 - cpu_busy_perc;
-		chartCPUCores.datasets[0].bars[1].value = 100 - cpu0_busy_perc;
-		chartCPUCores.datasets[0].bars[2].value = 100 - cpu1_busy_perc;
-		chartCPUCores.datasets[0].bars[3].value = 100 - cpu2_busy_perc;
-		chartCPUCores.datasets[0].bars[4].value = 100 - cpu3_busy_perc;
-		//busy
-		chartCPUCores.datasets[1].bars[0].value = cpu_busy_perc;
-		chartCPUCores.datasets[1].bars[1].value = cpu0_busy_perc;
-		chartCPUCores.datasets[1].bars[2].value = cpu1_busy_perc;
-		chartCPUCores.datasets[1].bars[3].value = cpu2_busy_perc;
-		chartCPUCores.datasets[1].bars[4].value = cpu3_busy_perc;
-		
-		chartCPUCores.update();
-		//RAM
-		var free = parseInt(val.ram[1]);
-		var occ = val.ram[0] - val.ram[1];
-		console.log("RAM: free: " + free + " MiB, occ.: " + occ + " MiB");
-		chartRam.segments[0].value = free;
-		chartRam.segments[1].value = occ;
-		chartRam.update();
-		//Root disk
-		var free = parseInt(val.root[1]);
-		var occ = parseInt(val.root[2]);
-		console.log("Disk: free: " + free + " MiB, disk occ.: " + occ + " MiB");
-		chartDisk.segments[0].value = occ;
-		chartDisk.segments[1].value = free;
-		chartDisk.update();
-		//SDA1 disk
-		var free = parseInt(val.sda[1]);
-		var occ = parseInt(val.sda[2]);
-		console.log("Disk: free: " + free + " MiB, disk occ.: " + occ + " MiB");
-		chartSDA.segments[0].value = occ;
-		chartSDA.segments[1].value = free;
-		chartSDA.update();
-		//Network
-		var date = pad(d.getHours(), 2).concat(":", pad(d.getMinutes(), 2), ":", pad(d.getSeconds(), 2));
-		var inData = parseInt(val.network[0] * 100)/ 100;
-		var outData = parseInt(val.network[2] * 100) / 100;
-		console.log("In: " + inData + " KiB/s, out: " + outData + " KiB/s");
-		chartNetwork.addData([inData, outData], date);
-		if(chartNetwork.datasets[0].points.length > amountBig)
-			chartNetwork.removeData();
-		
-		var totalIn = (parseInt((val.network[1]/1024/1024) * 1000) / 1000);
-		var totalOut = (parseInt((val.network[3]/1024/1024) * 1000) / 1000);
-		$("#total_in").html("Total in: ".concat(totalIn, " GiB"));
-		$("#total_out").html("Total out: ".concat(totalOut, " GiB"));
-	});
+		$.get("stats.php", function(val)
+		{
+			val = $.parseJSON(val);
+			var d = new Date();
+			//temperature
+			var date = pad(d.getHours(), 2).concat(":", pad(d.getMinutes(), 2), ":", pad(d.getSeconds(), 2));
+			var temp = [(parseInt(val.temp / 10)) / 100];
+			console.log("Temperature: " + temp + " deg C");
+			chartTemp.addData(temp, date);
+			if(chartTemp.datasets[0].points.length > amountBig)
+				chartTemp.removeData();
+			//CPU
+			var cpu = val.cpu[0];
+			var cpu0 = val.cpu[1];
+			var cpu1 = val.cpu[2];
+			var cpu2 = val.cpu[3];
+			var cpu3 = val.cpu[4];
+			
+			var cpu_busy = (parseInt(cpu[1]) + parseInt(cpu[2]) + parseInt(cpu[3]));
+			var cpu_idle = parseInt(cpu[4]);
+			var cpu_busy_perc = parseInt(cpu_busy / (cpu_idle + cpu_busy) * 100);
+			var cpu0_busy = (parseInt(cpu0[1]) + parseInt(cpu0[2]) + parseInt(cpu0[3]));
+			var cpu0_idle = parseInt(cpu0[4]);
+			var cpu0_busy_perc = parseInt(cpu0_busy / (cpu0_idle + cpu0_busy) * 100);
+			var cpu1_busy = parseInt(cpu1[1]) + parseInt(cpu1[2]) + parseInt(cpu1[3]);
+			var cpu1_idle = parseInt(cpu1[4]);
+			var cpu1_busy_perc = parseInt(cpu1_busy / (cpu1_idle + cpu1_busy) * 100);
+			var cpu2_busy = parseInt(cpu2[1]) + parseInt(cpu2[2]) + parseInt(cpu2[3]);
+			var cpu2_idle = parseInt(cpu2[4]);
+			var cpu2_busy_perc = parseInt(cpu2_busy / (cpu2_idle + cpu2_busy) * 100);
+			var cpu3_busy = parseInt(cpu3[1]) + parseInt(cpu3[2]) + parseInt(cpu3[3]);
+			var cpu3_idle = parseInt(cpu3[4]);
+			var cpu3_busy_perc = parseInt(cpu3_busy / (cpu3_idle + cpu3_busy) * 100);
+			
+			console.log("cpu: " + cpu_busy_perc + "% | " + "cpu0: " + cpu0_busy_perc + "% | " + "cpu1: " + cpu1_busy_perc + "% | " + "cpu2: " + cpu2_busy_perc + "% | " + "cpu3: " + cpu3_busy_perc + "%");
+			//idle
+			chartCPUCores.datasets[0].bars[0].value = 100 - cpu_busy_perc;
+			chartCPUCores.datasets[0].bars[1].value = 100 - cpu0_busy_perc;
+			chartCPUCores.datasets[0].bars[2].value = 100 - cpu1_busy_perc;
+			chartCPUCores.datasets[0].bars[3].value = 100 - cpu2_busy_perc;
+			chartCPUCores.datasets[0].bars[4].value = 100 - cpu3_busy_perc;
+			//busy
+			chartCPUCores.datasets[1].bars[0].value = cpu_busy_perc;
+			chartCPUCores.datasets[1].bars[1].value = cpu0_busy_perc;
+			chartCPUCores.datasets[1].bars[2].value = cpu1_busy_perc;
+			chartCPUCores.datasets[1].bars[3].value = cpu2_busy_perc;
+			chartCPUCores.datasets[1].bars[4].value = cpu3_busy_perc;
+			
+			chartCPUCores.update();
+			//RAM
+			var free = parseInt(val.ram[1]);
+			var occ = val.ram[0] - val.ram[1];
+			console.log("RAM: free: " + free + " MiB, occ.: " + occ + " MiB");
+			chartRam.segments[0].value = free;
+			chartRam.segments[1].value = occ;
+			chartRam.update();
+			//Root disk
+			var free = parseInt(val.root[1]);
+			var occ = parseInt(val.root[2]);
+			console.log("Disk: free: " + free + " MiB, disk occ.: " + occ + " MiB");
+			chartDisk.segments[0].value = occ;
+			chartDisk.segments[1].value = free;
+			chartDisk.update();
+			//SDA1 disk
+			var free = parseInt(val.sda[1]);
+			var occ = parseInt(val.sda[2]);
+			console.log("Disk: free: " + free + " MiB, disk occ.: " + occ + " MiB");
+			chartSDA.segments[0].value = occ;
+			chartSDA.segments[1].value = free;
+			chartSDA.update();
+			//Network
+			var date = pad(d.getHours(), 2).concat(":", pad(d.getMinutes(), 2), ":", pad(d.getSeconds(), 2));
+			var inData = parseInt(val.network[0] * 100)/ 100;
+			var outData = parseInt(val.network[2] * 100) / 100;
+			console.log("In: " + inData + " KiB/s, out: " + outData + " KiB/s");
+			chartNetwork.addData([inData, outData], date);
+			if(chartNetwork.datasets[0].points.length > amountBig)
+				chartNetwork.removeData();
+			
+			var totalIn = (parseInt((val.network[1]/1024/1024) * 1000) / 1000);
+			var totalOut = (parseInt((val.network[3]/1024/1024) * 1000) / 1000);
+			$("#total_in").html("Total in: ".concat(totalIn, " GiB"));
+			$("#total_out").html("Total out: ".concat(totalOut, " GiB"));
+		});	
+	}
 }
 function updateDSL()
 {
-	$.post("dsl/index.php", {format: "json"}, function(data)
+	if(sync)
 	{
-		var object = $.parseJSON(data);
-		var dates = object.dates;
-		var ping = object.ping;
-		var dl = object.dl;
-		var ul = object.ul;
-		
-		chartDSL.datasets[0].points = new Array();
-		chartDSL.datasets[1].points = new Array();
-		chartDSLPing.datasets[0].points = new Array();
-		
-		for(var x = dates.length > amountDSL ? dates.length - amountDSL : 0; x < dates.length; x++)
+		$.post("dsl/index.php", {format: "json"}, function(data)
 		{
-			chartDSL.addData([dl[x], ul[x]], dates[x]);
-			chartDSLPing.addData([ping[x]], dates[x]);
-		}
-	});
+			var object = $.parseJSON(data);
+			var dates = object.dates;
+			var ping = object.ping;
+			var dl = object.dl;
+			var ul = object.ul;
+			
+			chartDSL.datasets[0].points = new Array();
+			chartDSL.datasets[1].points = new Array();
+			chartDSLPing.datasets[0].points = new Array();
+			
+			for(var x = dates.length > amountDSL ? dates.length - amountDSL : 0; x < dates.length; x++)
+			{
+				chartDSL.addData([dl[x], ul[x]], dates[x]);
+				chartDSLPing.addData([ping[x]], dates[x]);
+			}
+		});
+	}
 }
 
 function toggleSync()
 {
 	if($("#sync_on_off").hasClass("pause"))
 	{
+		sync = false;
 		$("#sync_on_off").removeClass("pause").addClass("continue");
 		$("#sync_on_off").html("Continue");
 		window.clearInterval(interval);
+		window.clearInterval(intervalDSL);
 	}
 	else
 	{
+		sync = true;
 		$("#sync_on_off").removeClass("continue").addClass("pause");
 		$("#sync_on_off").html("Pause");
 		update();
+		updateDSL();
 		interval = window.setInterval(update, 3000);
+		intervalDSL = window.setInterval(updateDSL, 900000);
 	}
 }
 function clock()
